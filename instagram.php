2e929ea2d69100617cfaf16795d51cad43da50ed
Steven Mann <steven.s.mann@gmail.com>
2016-02-14 02:09:39 +0000

    update

--- instagram.php
+++ instagram.php
@@ -1,68 +1,72 @@
-
-<section class="instagram">
-
-<? $posts = [] ?>
 <?php
-	function fetchData($url){
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
 
-		$result = curl_exec($ch);
+function pr($data)
+{
+    echo "<pre>";
+    print_r($data); // or var_dump($data);
+    echo "</pre>";
+}
 
-		curl_close($ch);
-		return $result;
-	}
-
-	$accessToken = "XXX";
-	$userId = "XXX";
-	$result = fetchData("https://api.instagram.com/v1/users/" . $userId . "/media/recent/?access_token=" . $accessToken);
-	$result = json_decode($result);
-	$count = 10;
 ?>
 
-<? try { ?>
+<?php
+	// instagram user id lookup here https://codeofaninja.com/tools/find-instagram-user-id
+	$instagram_uid = "XXX";
 
-	<? if($result!==false): ?>
-		<? for($i = 0; $i < $count; $i++) :?>
-			<? $post = $result->data[$i]; ?>
-			<? $caption = (isset($post->caption->text)) ? $post->caption->text : false ;?>
-			<? $likes = (isset($post->likes->count)) ? $post->likes->count : false ;?>
-			<? $posts[(int)$post->created_time] = [
-				'content'=>$post->images->standard_resolution->url,
-				'likes'=>$post->likes->count,
-				'url'=>$post->link,
-				'caption'=>$caption,
-			] ?>
-		<? endfor; ?>
-	<? endif; ?>
+	// instruction to get your access token here https://www.codeofaninja.com/2015/05/get-instagram-access-token.html
+	$access_token = "XXX.XXX.XXXXX";
+	$photo_count = 500;
 
-<? } catch (Exception $e) { ?>
-	<p>Instagram Feed is Down</p>
-<? } ?>
+	$json_link = "https://api.instagram.com/v1/users/{$instagram_uid}/media/recent/?";
+	$json_link .= "access_token={$access_token}&count={$photo_count}";
 
-<?
-	$i = 0;
-	foreach ($posts as $time => $post):
-		$i++;
-		if ($i <= $count) {
-?>
+	$json = file_get_contents($json_link);
+	$obj = json_decode($json, true, 1024);
 
-	<article class="post-wrap">
-		<a href="<?= $post['url'] ?>" target="_blank">
-			<img src="url('<?= $post['content'] ?>')">
+	$tags_array = [];
 
-			<? if ($post['likes']): ?>
-				<p class="likes-wrap--likes"><?= $post['likes'] ?>
-			<? endif; ?>
+	foreach ($obj['data'] as $post):
 
-			<? if ($post['caption']): ?>
-				<p class="post__instagram--caption"><?= substr($post['caption'], 0, 45) ?>...</p>
-			<? endif; ?>
+		$insta_img = str_replace("http://", "https://", $post['images']['standard_resolution']['url']);
+		$insta_link = $post['link'];
+
+		$insta_text = (isset($post['caption']['text'])) ? $post['caption']['text'] : false;
+		$insta_likes = (isset($post['likes']['count'])) ? $post['likes']['count'] : false;
+		$insta_username = $post['user']['username'];
+
+		$insta_date = date("F j, Y", $post['created_time']);
+
+		$insta_tags = (isset($post['tags'])) ? $post['tags'] : false;
+
+		// pr($insta_tags);
+		array_push($tags_array, $insta_tags);
+		$flat_tags_array = call_user_func_array('array_merge', $tags_array);
+		// die;
+
+		?>
+
+		<a href="<?php echo $insta_link ?>" target="_blank" class="insta__wrap" data-tags="<?php echo implode(" ",$insta_tags); ?>">
+
+			<img data-original="<?php echo $insta_img ?>" alt="Instagram <?php echo $insta_username . ' ' . $insta_text ?>" class="insta__img">
+
+			<noscript>
+				<img src="<?php echo $insta_img ?>" alt="Instagram <?php echo $insta_username . ' ' . $insta_text ?>" class="insta__img">
+			</noscript>
+
+			<div class="insta__overlay">
+
+				<p class="insta__copy"><?php echo $insta_text ?></p>
+				<p class="insta__date"><?php echo $insta_date ?></p>
+
+			</div>
+
 		</a>
-	</article>
 
-		<? } ?>
-	<? endforeach ?>
-</section>
+
+
+		<?php
+	endforeach;
+	$unique_tags_array = array_unique($flat_tags_array);
+	// pr($unique_tags_array);
+
+?>
